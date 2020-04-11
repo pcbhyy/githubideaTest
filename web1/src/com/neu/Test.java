bbf379db97b62bf0f3fa1467661eae9fc073f309
pcbhyy <pcbhyy@163.com>
2020-04-11 11:54:17 +0800

    four

--- web1/src/com/neu/Test.java
+++ web1/src/com/neu/Test.java
@@ -10,5 +10,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("second");
+        System.out.println("three");
+        System.out.println("four");
     }
 }
